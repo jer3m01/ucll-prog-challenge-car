cf03ac90ca08f5abfcc660f091f83d04653ed1e0
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-09-07 19:51:03 +0200

    removed car-english

--- labs/src/car-english/components/index.ts
+++
@@ -1,4 +0,0 @@
-import { CarSimulationSummary } from './car-simulation-summary';
-import { CarSimulationViewer } from './car-simulation-viewer';
-
-export { CarSimulationSummary, CarSimulationViewer };
